7d9c4a6c95d4cdf32fa8217cd8c397f975f486d3
Taeshmello <taeho060419@gmail.com>
2024-09-19 17:36:45 +0900

    at rt 부분 수정

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,11 @@ import FaqManage from './pages/FAQ/FaqManage';
 import SiginUp from './pages/SignUp/SiginUp';
 import Department from './pages/Department/Department';
 import Category from './pages/Category/Category';
-import { CookiesProvider, useCookies } from 'react-cookie';
-import { useEffect } from 'react';
-import { refreshAccessToken } from './utils/api'; // Access Token 갱신 함수 import
+import { CookiesProvider} from 'react-cookie';
+
 import Waiting from './pages/Waiting';
 
 function App() {
-  const [cookies, setCookie, removeCookie] = useCookies(['accessToken']);
-
-  // Access Token 갱신을 주기적으로 수행
-  useEffect(() => {
-    const interval = setInterval(async () => {
-      if (cookies.accessToken) {
-        // 토큰이 존재할 때 갱신 시도
-        await refreshAccessToken(setCookie);
-      }
-    }, 14 * 60 * 1000); // 14분마다 실행 (15분 만료 기준)
-
-    return () => clearInterval(interval); // 컴포넌트 언마운트 시 정리
-  }, [cookies.accessToken, setCookie, removeCookie]);
   
   return (
     <CookiesProvider>
