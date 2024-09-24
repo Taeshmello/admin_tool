1aa9e244e181df7bc2e776e34900f02030f47c5c
Taeshmello <taeho060419@gmail.com>
2024-09-24 18:02:04 +0900

    최종 module.css

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,20 +1,16 @@
-import './MainPage.css'
-
+import styles from './MainPage.module.css';
 
 const MainPage = () => {
-
   return (
-    <div className="page-container">
-      <div className="page-content">
-        <div className='title-container'>
-          VALOFE ADMIN TOOL 
-          <h5>Control pannel</h5>
+    <div className={styles.pageContainer}>
+      <div className={styles.pageContent}>
+        <div className={styles.titleContainer}>
+          <h1>VALOFE ADMIN TOOL</h1> 
+          <h5>Control panel</h5>
         </div>
-
-
       </div>
     </div>
-  )
+  );
 }
 
 export default MainPage;
