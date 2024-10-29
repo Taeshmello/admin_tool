f346c0e26028a9d294ab9abbbdbe8199b12d06e9
Taeshmello <taeho060419@gmail.com>
2024-10-29 16:19:15 +0900

    Not found 페이지 문구 수정

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const NotFound: React.FC = () => {
     return (
         <div className={notFoundStyles.container}>
-            <h1 className={notFoundStyles.title}>404</h1>
+            <h1 className={notFoundStyles.title}>Page Not Found</h1>
             <p className={notFoundStyles.message}>이 페이지는 존재하지 않습니다.</p>
             <p className={notFoundStyles.secretMessage}>이 페이지는 우주로 사라졌습니다!</p>
             <a href="/" className={notFoundStyles.spaceshipButton}>홈으로 돌아가기</a>
