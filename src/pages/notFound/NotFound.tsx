adb564d17f6c3b94754755290194fb2a02badbdf
Taeshmello <taeho060419@gmail.com>
2024-10-29 15:50:05 +0900

    notfound 페이지 수정

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -6,7 +6,7 @@ export const NotFound: React.FC = () => {
         <div className={notFoundStyles.container}>
             <h1 className={notFoundStyles.title}>404</h1>
             <p className={notFoundStyles.message}>이 페이지는 존재하지 않습니다.</p>
-            <p className={notFoundStyles.secretMessage}>이 페이지는 우주에 사라졌습니다!</p>
+            <p className={notFoundStyles.secretMessage}>이 페이지는 우주로 사라졌습니다!</p>
             <a href="/" className={notFoundStyles.spaceshipButton}>홈으로 돌아가기</a>
             {Array.from({ length: 20 }).map((_, index) => (
                 <div key={index} className={`${notFoundStyles.star} ${notFoundStyles[`star${index}`]}`}></div>
